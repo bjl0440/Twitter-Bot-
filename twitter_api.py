3476be64210025b6593135ebd10231cf7434403c
Brian Liu <bjl.0440@gmail.com>
2022-05-12 21:24:17 -0700

    init

---
+++ twitter_api.py
@@ -0,0 +1,68 @@
+import tweepy
+import configparser
+import pandas as p
+
+# compile_trendss trend_data about trending topics into list 
+def compile_trends(trends, trend_data):
+    for trend in trends[0]['trends']:
+        if trend['tweet_volume'] is not None and trend['tweet_volume'] > 10000:
+            trend_data.append((trend['tweet_volume'],trend['name'],trend['url']))
+
+    trend_data.sort(reverse = True, key = lambda x: x[0])
+    return trend_data
+
+# compiles tweets about a trend
+def compile_tweets(trend, tweet_data):
+    tweets = tweepy.Cursor(api.search_tweets, q = trend[1],lang = 'en' ,  count = 200, tweet_mode = 'extended').items(200)
+    tweet_data.append((trend))
+    for tweet in tweets:
+        tweet_data.append((tweet.user.screen_name, tweet.full_text))
+    
+
+# read Configs from ini file
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+api_key = config['twitter']['api_key']
+api_key_secret = config['twitter']['api_key_secret']
+
+access_token = config['twitter']['access_token']
+access_token_secret = config['twitter']['access_token_secret']
+
+# tweepy authentication
+auth = tweepy.OAuth1UserHandler(api_key, api_key_secret, access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# trends based on location
+trends = api.get_place_trends(id = 23424775)
+
+# tweets
+
+
+# trend_dataframe lists
+trends_column = ['tweet_volume','Topic','URL']
+trend_data = []
+tweet_column = ['User','Tweet']
+tweet_data = []
+compile_trends(trends,trend_data)
+
+
+
+#print(trends)
+#print(compile_trends(trends,trend_data))
+
+# converts trend_data into csv file
+# top 10 tweeted trending
+trend_data = trend_data[0:1]
+print(trend_data)
+
+
+#trend_dataframe = p.DataFrame(trend_data, columns=trends_column)
+#trend_dataframe.to_csv('trending_topics.csv')
+
+
+for trend in trend_data:
+    compile_tweets(trend,tweet_data)
+    tweet_dataframe = p.DataFrame(tweet_data, columns = tweet_column)
+
+tweet_dataframe.to_csv('tweets.csv')
