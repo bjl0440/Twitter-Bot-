5971d57d42075d84efa77f1340e87b67b6737041
Brian Liu <bjl.0440@gmail.com>
2022-05-13 11:23:10 -0700

    new

--- nlp/twt_sentiment.py
+++ nlp/twt_sentiment.py
@@ -1,11 +1,13 @@
 import pandas as pd
 
 #df = open("csv/tweets.csv", "r")
-tweets_df = pd.read_csv('csv/tweets.csv')
+tweets_df = pd.read_csv('csv/tweets.csv',index_col=0)
 tweets_df = tweets_df.reset_index()
 
 tweet_number = 0
 
-print(tweets_df)
-# for tweet in tweets_df.iterrows():
-#     print(tweet['Tweet'].dtype)
+#print(tweets_df)
+index = 1
+for tweet in tweets_df.iterrows():
+    print(tweet[1])
+    index +=1
