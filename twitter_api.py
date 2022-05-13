a9df122cf1d87e482602f35d85837a8a06c098e5
Brian Liu <bjl.0440@gmail.com>
2022-05-12 23:19:44 -0700

    new

--- twitter_api.py
+++ twitter_api.py
@@ -1,7 +1,7 @@
 import tweepy
 import configparser
 import pandas as p
-
+import os
 # compile_trendss trend_data about trending topics into list 
 def compile_trends(trends, trend_data):
     for trend in trends[0]['trends']:
@@ -13,14 +13,15 @@ def compile_trends(trends, trend_data):
 
 # compiles tweets about a trend
 def compile_tweets(trend, tweet_data):
-    tweets = tweepy.Cursor(api.search_tweets, q = trend[1],lang = 'en' ,  count = 200, tweet_mode = 'extended').items(200)
+    tweets = tweepy.Cursor(api.search_tweets, q = trend[1] + '-filter:retweets -filter:replies',lang = 'en' 
+    ,count = 200, tweet_mode = 'extended').items(200)
 
     for tweet in tweets:
-        tweet_data.append((tweet.user.screen_name, tweet.full_text))
+        tweet_data.append((tweet.created_at,tweet.user.screen_name, tweet.full_text))
     
 # read Configs from ini file
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read('config.ini.gitignore')
 
 api_key = config['twitter']['api_key']
 api_key_secret = config['twitter']['api_key_secret']
@@ -41,7 +42,7 @@ trends = api.get_place_trends(id = 23424775)
 # trend_dataframe lists
 trends_column = ['tweet_volume','Topic','URL']
 trend_data = []
-tweet_column = ['User','Tweet']
+tweet_column = ['Time_tweeted','User','Tweet']
 tweet_data = []
 compile_trends(trends,trend_data)
 
@@ -52,6 +53,8 @@ trend_data = trend_data[0:2]
 trend_dataframe = p.DataFrame(trend_data, columns=trends_column)
 trend_dataframe.to_csv('trending_topics.csv')
 
+# compiles tweets into csv file
+os.remove('tweets.csv') #removes old csv file
 for trend in trend_data:
     compile_tweets(trend,tweet_data)
     tweet_dataframe = p.DataFrame(tweet_data, columns = tweet_column)
